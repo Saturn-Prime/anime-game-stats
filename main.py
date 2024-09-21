87b244f4ad21113a0e19759758b77aa434475424
Saturn-Prime <117035926+Saturn-Prime@users.noreply.github.com>
2024-09-20 23:00:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class ZZZRes:
 
 
 def format_date(date: datetime) -> str:
-    tz = pytz.timezone("America/New_York")
+    tz = pytz.timezone("Asia/Jakarta")
     now = date.now(tz=tz)
     return f"{now.strftime('%b')} {now.strftime('%d')}, {now.strftime('%Y')} {now.strftime('%H:%M %z')}"
 
@@ -122,17 +122,17 @@ class AnimeGame(genshin.Client):
     def _get_character_showcase(self, game: genshin.Game, uid: int) -> Optional[List[str]]:
         logger.info("Getting character showcase")
         url = f"{MBB_API}/{'genshin' if game == GENSHIN else 'hsr'}_card?uid={uid}"
-        res = requests.get(url).json()
-        if res["status"] == 200:
-            return save_images(res["result"])
+        res = requests.get(url)
+        if res.status_code == 200:
+            return save_images(res.json()["result"])
         return None
 
     def _get_user_profile(self, game: genshin.Game, uid: int) -> Optional[str]:
         logger.info("Getting user profile")
         url = f"{MBB_API}/{'genshin' if game == GENSHIN else 'hsr'}_profile?uid={uid}"
-        res = requests.get(url).json()
-        if res["status"] == 200:
-            return save_images(res["result"], _type="profile")
+        res = requests.get(url)
+        if res.status_code == 200:
+            return save_images(res.json()["result"], _type="profile")
         return None
 
     async def get_genshin_res(self) -> GenshinRes | None:
