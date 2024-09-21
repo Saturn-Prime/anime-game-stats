beb40057202bc6e41f281066c1730b8c474573f4
Saturn-Prime <117035926+Saturn-Prime@users.noreply.github.com>
2024-09-20 23:02:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class ZZZRes:
 
 
 def format_date(date: datetime) -> str:
-    tz = pytz.timezone("Asia/Jakarta")
+    tz = pytz.timezone("America/New_York")
     now = date.now(tz=tz)
     return f"{now.strftime('%b')} {now.strftime('%d')}, {now.strftime('%Y')} {now.strftime('%H:%M %z')}"
 
